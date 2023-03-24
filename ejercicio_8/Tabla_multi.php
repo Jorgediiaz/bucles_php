4e090334143ecaca0ce8ee3244c3fef1f0b55a4d
Jorgediiaz <122317222+Jorgediiaz@users.noreply.github.com>
2023-03-24 11:11:46 +0100

    Create Tabla_multi.php

---
+++ ejercicio_8/Tabla_multi.php
@@ -0,0 +1,15 @@
+<?php
+var dump($_GET);
+$numero = $_GET("numero");
+echo "<h1> La tabla del $numero:</h1>";
+
+echo "<table>";
+ for($i=0; $i<=10; $i=$i++){
+    $res=$numero*$i;
+    echo "<tr>";
+    echo "<td>$numero</td><td>x</td><td>$i</td><td>=</td><td>$res</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
