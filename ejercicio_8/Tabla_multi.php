f78a417c7792f4ee2742d73c2c467e9ddf43d856
Jorgediiaz <122317222+Jorgediiaz@users.noreply.github.com>
2023-03-24 11:14:46 +0100

    Update Tabla_multi.php

--- ejercicio_8/Tabla_multi.php
+++ ejercicio_8/Tabla_multi.php
@@ -7,7 +7,7 @@ echo "<table>";
  for($i=0; $i<=10; $i=$i++){
     $res=$numero*$i;
     echo "<tr>";
-    echo "<td>$numero</td><td>x</td><td>$i</td><td>=</td><td>$res</td>";
+    echo "<td>$numero</td>"."<td>x</td>"."<td>$i</td>"."<td>=</td>"."<td>$res</td>";
     echo "</tr>";
 }
 echo "</table>";
